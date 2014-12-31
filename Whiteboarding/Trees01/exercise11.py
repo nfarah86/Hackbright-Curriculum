9a465b2bc7e763b9176fd800d56bf240e4e7b98c
Nadine <nfarah86@gmail.com>
2014-12-31 13:19:31 -0800

    starting working on binary trees

--- Whiteboarding/Trees01/exercise11.py
+++ Whiteboarding/Trees01/exercise11.py
@@ -1,24 +1,56 @@
 class BinaryTreeNode:
-    def __init__(self, value):
-        pass
+    def __init__(self, root):
+        self.left = None
+        self.right = None
+        self.root = root
 
     def get_left(self):
-        pass
+        return self.left
 
     def set_left(self, node):
-        pass
+        self.left = node
+
+    def insert_left(self, newNode):
+        tree = BinaryTreeNode(newNode)
+        if self.left == None:
+            self.left = tree
+        else:
+            self.left = tree
+            tree.left = self.left
 
     def get_right(self):
-        pass
+        return self.right
 
     def set_right(self, node):
-        pass
+        self.right = node
+
+    def insert_right(self, newNode):
+        tree = BinaryTreeNode(newNode)
+        if self.right == None:
+            self.right = tree
+        else:
+            tree.right = self.right
+            self.right = tree
 
     def get_value(self):
-        pass
+        return self.root
 
-    def set_value(self, number):
-        pass
+    def set_value(self, newRoot):
+        self.root = newRoot
+
+def print_Tree(tree):
+    if tree != None:
+        print_Tree(tree.get_left())
+        print(tree.get_value())
+        print_Tree(tree.get_right())
+
 
 def depth_first_traversal(node):
     pass
+
+
+tempBTN = BinaryTreeNode(3)
+tempBTN.insert_left("bob")
+tempBTN.insert_right("karen")
+#tempBTN.set_right("nadine")
+print_Tree(tempBTN)
